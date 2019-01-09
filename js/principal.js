3b693d34c77a9132c0d5992c4f56186e8a948451
Marc <marc.gonzalez.sds@gmail.com>
2019-01-09 13:22:49 +0100

    sec

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,11 @@
 var temps = 10;
-var preguntaActual = null;
-var posPregunta = 0;
+var preguntaActual;
+var posPregunta;
+var maxPreguntes;
+var intTemps = null;
 
-window.onload = function(){
-    MostraPregunta();
-}
 
-function crearPregunta(id, pregunta, resposta1, resposta2){
+function crearPregunta(id, pregunta, resposta1, resposta2) {
     return {
         id: id,
         pregunta: pregunta,
@@ -17,25 +16,34 @@ function crearPregunta(id, pregunta, resposta1, resposta2){
 
 var preguntes = [
     crearPregunta(0, "Com es diu el creador del joc?", "Marc", "Pedro"),
-    crearPregunta(1, "Com es diu el creador del joc?", "Marc", "Pedro"),
-    crearPregunta(2, "Com es diu el creador del joc?", "Marc", "Pedro")
+    crearPregunta(1, "Com es diu el professor?", "Test", "Antionio"),
+    crearPregunta(2, "Que ha utilitzat per guardar les respostes?", "Cookies", "Variables")
 ]
 
+function test() {
+    posPregunta = 0;
+    preguntaActual = null;
+    maxPreguntes = preguntes.length;
 
-function MostraPregunta(){
+    MostraPregunta();
+}
+
+function MostraPregunta() {
     preguntaActual = preguntes[posPregunta];
 
     $("#pregunta").html(preguntaActual.pregunta);
+    $("#codi").html(preguntaActual.id);
     $("#textResposta1").html(preguntaActual.resposta1);
     $("#textResposta2").html(preguntaActual.resposta2);
 
-    setInterval(function(){
-        $("#temps").html(temps);
+    $("#temps").html(temps);
+    intTemps = setInterval(function () {
+        $("#temps").html(temps - 1);
 
         temps--;
 
-        if (temps == 0){
-            clearInterval(this);
+        if (temps == 0) {
+            clearInterval(intTemps);
 
             var resposta = getSeleccionat("resposta");
 
@@ -44,37 +52,41 @@ function MostraPregunta(){
     }, 1000);
 }
 
-function ValidarRespuesta(codiPregunta, codiResposta){
+function ValidarRespuesta(codiPregunta, codiResposta) {
     var respostaCorrecta = getCookie("Pregunta" + codiPregunta);
 
     var resultat = $("#resultat");
-    if (respostaCorrecta == codiResposta){
+    if (respostaCorrecta == codiResposta) {
         resultat.html("Correcte");
-    } else{
+    } else {
         resultat.html("Incorrecte");
     }
 
-    setTimeout(function(){
+    setTimeout(function () {
         temps = 10;
-        MostraPregunta();
-    }, 4000);
-}
+        posPregunta++;
 
-function test(){
+        resultat.html("");
 
+        if (maxPreguntes > posPregunta) {
+            MostraPregunta();
+        } else{
+            alert("Joc finalitzat!");
+        }
+    }, 1000);
 }
 
 function getSeleccionat(nom) {
     var seleccionat;
-  
+
     var llistat = document.getElementsByName(nom);
-  
+
     for (var i = 0; i < llistat.length && !seleccionat; i++) {
-      var actual = llistat[i];
-      if (actual.checked == true) {
-        seleccionat = actual.value;
-      }
+        var actual = llistat[i];
+        if (actual.checked == true) {
+            seleccionat = actual.value;
+        }
     }
-  
+
     return seleccionat;
-  }
+}
